3411bd039710d0eeb0521f8daef5b852a32d852f
cezaniltom <cezaniltom@outlook.com>
2023-09-25 13:05:32 -0300

    Atualização com processamento assíncrono

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
-console.log ('Sucesso!')
+
+
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+fetch(url)
+   /*retorno da promise*/ 
+   .then((response) => response.json()) //informa que o then dessa function está retornando uma promise de any
+
+   /*retorno do primeiro then*/
+    .then((jsonBody) => console.log(jsonBody)) //informa que o then dessa function é a partir de uma promise de any
+
+    .catch((error) => console.error(error))
+    
+// => Significa arrow  function
