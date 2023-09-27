d56e6daeed4ceced4b5a5e7f67f6ff86c8ee0547
cezaniltom <cezaniltom@outlook.com>
2023-09-27 12:17:22 -0300

    Listagem de itens da pokedex

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,35 @@
 
 
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+function convertPokemonToLi(pokemon) {
+    return `
+    <li class="pokemon">
+        <span class="number">#001</span>
+        <span class="name">${pokemon.name}</span>
+        
+        <div class="detail">
+            <ol class="types">
+                <li class="type">grass</li>
+                <li class="type">poison</li>
+            </ol>
 
-fetch(url)
-   /*retorno da promise*/ 
-   .then((response) => response.json()) //informa que o then dessa function está retornando uma promise de any
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+        </div>
+    </li>  
+    `
+}
 
-   /*retorno do primeiro then*/
-    .then((jsonBody) => console.log(jsonBody)) //informa que o then dessa function é a partir de uma promise de any
+const pokemonList = document.getElementById('pokemonList')
 
-    .catch((error) => console.error(error))
+pokeApi.getPokemons().then((pokemons) => {
+        const listItems = []    
+
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            listItems.push(convertPokemonToLi(pokemon))
+        }
+
+        console.log(listItems)
+        
+    })
     
-// => Significa arrow  function
+    
